0dec750351223eecce46929fe6b49ad082dcdcc5
jeeteshchattraband <jeeteshchattraband@gmail.com>
2019-03-22 11:21:15 +0530

    Shooting Battle Game first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAqps5e9pibB9L6b6xVzb6Nul1FPEbcR8Q',
+    authDomain: 'battle-game-d654f.firebaseapp.com',
+    databaseURL: 'https://battle-game-d654f.firebaseio.com',
+    projectId: 'battle-game-d654f',
+    storageBucket: 'battle-game-d654f.appspot.com',
+    messagingSenderId: '322259193300'
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAqps5e9pibB9L6b6xVzb6Nul1FPEbcR8Q',
+    authDomain: 'battle-game-d654f.firebaseapp.com',
+    databaseURL: 'https://battle-game-d654f.firebaseio.com',
+    projectId: 'battle-game-d654f',
+    storageBucket: 'battle-game-d654f.appspot.com',
+    messagingSenderId: '322259193300'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
